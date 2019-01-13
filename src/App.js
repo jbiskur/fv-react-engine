257922f1396eae374c07a61375b1d295571632fe
Julius Biskopstoe <jbiskopstoe@gmail.com>
2019-01-13 20:44:40 +0000

    added tests

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const App = () => {
-  console.log("HELLO");
-  return <h1>Hello there World!</h1>;
+const App = props => {
+  return <button onClick={props.onClick}>Hello there World!</button>;
+};
+
+App.propTypes = {
+  onClick: PropTypes.func
 };
 
 export default App;
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,38 @@
 import React from "react";
 import App from "./../App";
+import constructBuilder from "test-component-builder";
+import { render, fireEvent, cleanup } from "react-testing-library";
 
+const builder = props => {
+  return <App {...props} />;
+};
+
+const jestfn = jest.fn();
+
+let app;
 describe("App", () => {
+  afterEach(cleanup);
+
+  beforeEach(() => {
+    app = constructBuilder(builder)
+      .with({
+        onClick: jestfn
+      })
+      .using(render);
+  });
+
   it("Should run without crashing", () => {
-    const app = <App />;
-    expect(app).toBeTruthy();
+    expect(app.create()).toBeTruthy();
+  });
+
+  it("should have a button", () => {
+    const { getByText } = app.create();
+    expect(getByText("Hello there World!")).toBeTruthy();
+  });
+
+  it("should call method when clicking on button", () => {
+    const { getByText } = app.create();
+    fireEvent.click(getByText("Hello there World!"));
+    expect(jestfn).toHaveBeenCalledTimes(1);
   });
 });
